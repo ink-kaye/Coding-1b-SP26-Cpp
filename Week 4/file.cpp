8d15c3ef5edb631451ee9950c15ea717053c7d3d
Keira <kpvilaysing@gmail.com>
2026-04-21 12:08:28 -0500

    learned to reference different files

---
+++ Week 4/file.cpp
@@ -0,0 +1,15 @@
+// Keira Vilaysing Coding 1 2026
+// writing to and reading from files
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream> //file stream
+
+using namespace std;
+
+int main() {
+    cout << "Let's write to a file!\n";
+
+    return 0;
+}
